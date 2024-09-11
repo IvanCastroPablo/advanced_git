7d219174ed4bdaa182c27aa09f4cba37f80cd8df
IvanCastroPablo <ivancastropablo@hotmail.com>
2024-09-11 15:06:32 +0200

    Commit changes in new branch "hotfix"

--- main.sh
+++ main.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "Aplicación general"
+
+add_two() {
+	local num1=2
+	local num2=2
+	local sum=$((num1 + num2))
+	echo "The sum of $num1 and $num2 is $sum"
+}
+
+echo "Calculating 2 + 2 ..."
+add_two
+
+echo "New change from hotfix branch"
